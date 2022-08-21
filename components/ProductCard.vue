67fcf86a3b5d32047fbd2257752c2afc76a9862a
rejohnbd <rejohn.cse@gmail.com>
2022-08-22 03:29:29 +0600

    Fetch Data and loop in nuxt

--- components/ProductCard.vue
+++ components/ProductCard.vue
@@ -1,21 +1,19 @@
 <template>
     <div class="md:flex">
         <div class="md:flex-shrink-0">
-            <img 
-                class="rounded-lg md:w-56"
-                src="https://images.unsplash.com/photo-1488426862026-3ee34a7d66df?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
-                alt="woman" 
-            />
+            <img class="rounded-lg md:w-56" :src="product.image" :alt="product.title" />
         </div>
         <div class="mt-4 md:mt-0 md:ml-6">
             <div class="uppercase tracking-wide text-sm text-indigo-500 font-bold">
-                Marketing
+                {{ product.continent }}
             </div>
-            <a href="#" class="block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline">
-                Find Customers
-            </a>
+            <NuxtLink 
+                :to="product.slug"
+                class="block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline">
+                {{ product.title }}
+            </NuxtLink>
             <p class="mt-2 text-gray-600">
-                is another hook for universal data fetching. Unlike fetch, which requires you to set properties on the component instance (or dispatch Vuex actions)
+                {{ product.description }}
             </p>
         </div>
     </div>
@@ -23,6 +21,11 @@
 
 <script>
 export default {
-
+    props: {
+        product: {
+            type: Object,
+            default: () => {}
+        }
+    }
 }
 </script>
--- pages/products.vue
+++ pages/products.vue
@@ -1,11 +1,38 @@
 <template>
     <div class="container mx-auto mt-8">
-        <ProductCard />
+        <!-- <ProductCard :product="product" /> -->
+        <ProductCard 
+            v-for="product in products" 
+            :key="product.title" 
+            :product="product" 
+            class="mb-4"
+        />
     </div>
 </template>
 
 <script>
+
     export default {
-        name: 'ProductsPage'
+        name: 'ProductsPage',
+        data() {
+            return {
+                // product: {
+                //     title: 'title',
+                //     image: 'icon.png',
+                //     description: 'description',
+                //     continent: 'continent',
+                //     slug: '/'
+                // }
+                products: []
+            }
+        },
+
+        async fetch() {
+            this.products = await fetch(
+                'https://api.nuxtjs.dev/mountains'
+            ).then(res => res.json())
+        },
+    
     }
 </script>
+
