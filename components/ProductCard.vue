da414d702c0d1782f72a7718841274235789e342
rejohnbd <rejohn.cse@gmail.com>
2022-08-22 02:44:46 +0600

    Nuxt auto components

---
+++ components/ProductCard.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="md:flex">
+        <div class="md:flex-shrink-0">
+            <img 
+                class="rounded-lg md:w-56"
+                src="https://images.unsplash.com/photo-1488426862026-3ee34a7d66df?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+                alt="woman" 
+            />
+        </div>
+        <div class="mt-4 md:mt-0 md:ml-6">
+            <div class="uppercase tracking-wide text-sm text-indigo-500 font-bold">
+                Marketing
+            </div>
+            <a href="#" class="block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline">
+                Find Customers
+            </a>
+            <p class="mt-2 text-gray-600">
+                is another hook for universal data fetching. Unlike fetch, which requires you to set properties on the component instance (or dispatch Vuex actions)
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
---
+++ pages/products.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="container mx-auto mt-8">
+        <ProductCard />
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ProductsPage'
+    }
+</script>
