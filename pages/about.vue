b9efbc444125c9ab577db1c2bed4491768f5f5cf
rejohnbd <rejohn.cse@gmail.com>
2022-08-22 02:01:12 +0600

    Nuxt connection helpers offline & online

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,8 @@
 <template>
-    <div>
+    <div class="container">
         <h1>About</h1>
+        <p v-if="$nuxt.isOffline">Opps, you are offline.</p>
+        <p v-if="$nuxt.isOnline">Yeahhh, I am online.</p>
         <NuxtLink to="/">Home</NuxtLink>
     </div>
 </template>
