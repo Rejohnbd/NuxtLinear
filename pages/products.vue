75f8331003c4e8e0e664f84e63fd393a7ab39ab5
rejohnbd <rejohn.cse@gmail.com>
2022-08-23 02:05:17 +0600

    Lazy loading auto imported components in nuxt

--- pages/products.vue
+++ pages/products.vue
@@ -4,14 +4,30 @@
         <NuxtLink to="/" class="text-left text-gray-600 hover:underline">
             Home
         </NuxtLink>
+        <button 
+            class="text-sm bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded-full mb-4"
+            @click="showProducts"
+        >
+            Show Products
+        </button>
         
-        <ProductCard />
+        <LazyProductCard v-if="show"/>
     </div>
 </template>
 
 <script>
     export default {
-        name: 'ProductsPage'
+        name: 'ProductsPage',
+        data() {
+            return {
+                show: false
+            }
+        },
+        methods: {
+            showProducts() {
+                this.show = true;
+            }
+        }
     }
 </script>
 
